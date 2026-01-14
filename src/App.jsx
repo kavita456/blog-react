4a007ab44bea1007785b448ccd5ab9db553cae1e
kavita456 <kavitasinghlal@gmail.com>
2026-01-14 23:23:16 +0530

    component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
-function App(){
-
+function App() {
   return (
     <div>
-      <h1>first component</h1>
+      <h1>first component {add()}</h1>
+      <Fruit />
+      <Colour />
     </div>
-  )
+  );
+}
+
+function Fruit() {
+  return <div>this is my second componenet</div>;
+}
+
+function Colour() {
+  return <div> this colour is red</div>;
+}
+
+function add() {
+  return 7 + 8;
 }
 
 export default App;
