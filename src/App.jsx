5fba84d8e34e3fb42d2c67d53169bf84c6550650
kavita456 <kavitasinghlal@gmail.com>
2026-01-15 00:21:42 +0530

    component type , export, default export,named export

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
+import  Login,{Profile,Setting,country} from './UserComponent.jsx'
+
+
 function App() {
   return (
     <div>
-      <h1>first component {add()}</h1>
-      <Fruit />
-      <Colour />
+      <h1>first component </h1>
+     <Login/>
+     <Profile/>
+     <Setting/>
+     <div>{country}</div>
     </div>
   );
 }
 
-function Fruit() {
-  return <div>this is my second componenet</div>;
-}
-
-function Colour() {
-  return <div> this colour is red</div>;
-}
-
-function add() {
-  return 7 + 8;
-}
 
 export default App;
---
+++ src/UserComponent.jsx
@@ -0,0 +1,24 @@
+
+function Login(){
+    return (
+        <div>login User</div>
+    )
+}
+
+
+export default Login;
+ export function Profile(){
+    return (
+        <div>
+        <h1>profile</h1>
+        </div>
+    )
+}
+
+export function Setting(){
+    return(
+        <div>hello setting</div>
+    )
+}
+
+ export const  country="india";
